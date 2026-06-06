f7b482525dcd6ec4678809d7424df1a81a752f82
Farhad Qaunain <fqqaunain@gmail.com>
2026-06-06 18:06:40 +0530

    Remove commented console logs and unused share button code in ViewResume component

--- src/my-resume/[resumeId]/view/index.jsx
+++ src/my-resume/[resumeId]/view/index.jsx
@@ -12,22 +12,13 @@ function ViewResume() {
     const [resumeInfo,setResumeInfo]=useState();
     const {resumeId}=useParams();
 
-    console.log("Header", Header);
-    console.log("ResumePreview", ResumePreview);
-    console.log("Button", Button);
-
-    console.log("Resume Info:", resumeInfo);
-    console.log("rewebshare", RWebShare);
-    console.log("resumeId", resumeId);
-
     useEffect(()=>{
-        console.log("calling APi");
+        // console.log("calling APi");
         GetResumeInfo();
     },[])
     const GetResumeInfo=()=>{
-        console.log("inside getresueInfo");
         GlobalApi.GetResumeById(resumeId).then(resp=>{
-            console.log("Resume Info1:", resp.data.data);
+            // console.log("Resume Info1:", resp.data.data);
             setResumeInfo(resp.data.data);
         }).catch(err=>{
         console.log("API ERROR",err);
@@ -59,16 +50,6 @@ function ViewResume() {
                 title: resumeInfo?.firstName+" "+resumeInfo?.lastName+" resume",
                 })}; 
 
-                {/* <Button onClick={() => {
-                navigator.share({
-                    title: `${resumeInfo?.firstName} ${resumeInfo?.lastName} Resume`,
-                    text: "Hello Everyone, This is my resume",
-                    url: `https://ai-resume-builder-f9cf.onrender.com/my-resume/${resumeId}/view`
-                })
-                }}>
-                Share
-                </Button> */}
-
                 {resumeInfo && (
                 <RWebShare
                     data={{
@@ -80,25 +61,14 @@ function ViewResume() {
                     <Button>Share</Button>
                 </RWebShare>
                 )}
-               
-                {/* <RWebShare
-                    data={{
-                    text: "Hello Everyone, This is my resume please open url to see it",
-                    url: import.meta.env.VITE_BASE_URL+"/my-resume/"+resumeId+"/view",
-                    title: resumeInfo?.firstName+" "+resumeInfo?.lastName+" resume",
-                    }}
-                    onClick={() => console.log("shared successfully!")}
-                > <Button>Share</Button>
-                 {console.log("share url2", import.meta.env.VITE_BASE_URL+"/my-resume/"+resumeId+"/view")}
-                </RWebShare> */}
             </div>
         </div>
             
         </div>
         <div className='my-10 mx-10 md:mx-20 lg:mx-36'>
-            {/* <div id="print-area" >
+            <div id="print-area" >
                 <ResumePreview/>
-            </div> */}
+            </div>
         </div>
     </ResumeInfoContext.Provider>
   )
